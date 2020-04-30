3518661827ff3833fbec522f29b81aa6a318e3d3
dqs <18705147127@163.com>
2020-04-30 14:54:28 +0800

    隐藏联系客服

--- THJG/THJG/Module/Base/THJGConstants.swift
+++ THJG/THJG/Module/Base/THJGConstants.swift
@@ -37,14 +37,16 @@ let RSA_PUBLICK_KEY = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDYldTRQgmZP/Bcj+qn0
 //let baseUrl = "http://192.168.0.130:8099/info-app-thjg/core"
 //15测试
 //let baseUrl = "http://192.168.0.15:7112/core"
-//新测试
+//新测试-打包
+let baseUrl = "http://192.168.0.15:7112/core"
+//新测试-开发
 //let baseUrl = "http://58.213.46.226:9090/core"
 //正式环境
-let baseUrl = "https://yyapp.yunshows.com/core"
+//let baseUrl = "https://yyapp.yunshows.com/core"
 
 /* 环境 */
-//let ENVIRONMENT = "dev"
-let ENVIRONMENT = "prd"
+let ENVIRONMENT = "dev"
+//let ENVIRONMENT = "prd"
 
 /* 各种机型 */
 //屏幕宽度
